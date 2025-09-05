f9e00933f8659d24ad311f5d3cc722564a568137
dpcks0509 <dpcks0509@gmail.com>
2025-09-05 15:39:31 +0900

    feat: CalendarColors 추가

--- app/src/main/java/pusan/university/plato_calendar/presentation/common/theme/Color.kt
+++ app/src/main/java/pusan/university/plato_calendar/presentation/common/theme/Color.kt
@@ -9,3 +9,29 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val CalendarLavender = Color(0xFF7986CB)
+val CalendarSage = Color(0xFF33B679)
+val CalendarGrape = Color(0xFF8E24AA)
+val CalendarFlamingo = Color(0xFFE67C73)
+val CalendarBanana = Color(0xFFF6BF26)
+val CalendarTangerine = Color(0xFFF4511E)
+val CalendarPeacock = Color(0xFF039BE5)
+val CalendarGraphite = Color(0xFF616161)
+val CalendarBlueberry = Color(0xFF3F51B5)
+val CalendarBasil = Color(0xFF0B8043)
+val CalendarTomato = Color(0xFFD50000)
+
+val CalendarColors = listOf(
+    CalendarLavender,
+    CalendarSage,
+    CalendarGrape,
+    CalendarFlamingo,
+    CalendarBanana,
+    CalendarTangerine,
+    CalendarPeacock,
+    CalendarGraphite,
+    CalendarBlueberry,
+    CalendarBasil,
+    CalendarTomato
+)
