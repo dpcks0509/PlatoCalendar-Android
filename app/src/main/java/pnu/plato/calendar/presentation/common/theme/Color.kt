534752e77ce4cfc5552ba82eaf5b4ddbd8aacc3a
dpcks0509 <dpcks0509@gmail.com>
2025-09-11 00:44:14 +0900

    feat: MainColor 추가

--- app/src/main/java/pnu/plato/calendar/presentation/common/theme/Color.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/common/theme/Color.kt
@@ -19,6 +19,9 @@ val Pink40 = Color(0xFF7D5260)
 // val CalendarBasil = Color(0xFF0B8043)
 // val CalendarTomato = Color(0xFFD50000)
 
+val MainColor = Color(0xFF3B71CA)
+val DarkMainColor = Color(0xFF3B71CA)
+
 val CalendarSage = Color(0xFF33B679)
 val CalendarLavender = Color(0xFF7986CB)
 val CalendarGraphite = Color(0xFF616161)
