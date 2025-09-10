d8a54efaff5ab461d3a873daf2bd8f8c17260ecc
dpcks0509 <dpcks0509@gmail.com>
2025-09-11 00:44:07 +0900

    feat: color 수정

--- app/src/main/java/pnu/plato/calendar/presentation/calendar/model/AcademicScheduleUiModel.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/calendar/model/AcademicScheduleUiModel.kt
@@ -2,7 +2,7 @@ package pnu.plato.calendar.presentation.calendar.model
 
 import androidx.compose.ui.graphics.Color
 import pnu.plato.calendar.domain.entity.AcademicSchedule
-import pnu.plato.calendar.presentation.common.theme.CalendarSage
+import pnu.plato.calendar.presentation.common.theme.CalendarLavender
 import java.time.LocalDate
 
 data class AcademicScheduleUiModel(
@@ -15,6 +15,6 @@ data class AcademicScheduleUiModel(
         title = domain.title,
         startAt = domain.startAt,
         endAt = domain.endAt,
-        color = CalendarSage
+        color = CalendarLavender
     )
 }
