07e5c8b8d5fb2c667bff65538e0438433c1f3796
dpcks0509 <dpcks0509@gmail.com>
2025-09-14 13:30:00 +0900

    feat: color, theme 초기 설정

--- app/src/main/java/pnu/plato/calendar/presentation/common/theme/Color.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/common/theme/Color.kt
@@ -2,14 +2,17 @@ package pnu.plato.calendar.presentation.common.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor = Color(0xFF3B71CA)
+val OnPrimaryColor = Color(0xFFFFFFFF)
+val BackgroundColor = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimaryColor = Color(0xFF3B71CA)
+val DarkOnPrimaryColor = Color(0xFFF5F5F5)
+val DarkBackgroundColor = Color(0xFF121212)
 
+val CalendarSage = Color(0xFF33B679)
+val CalendarLavender = Color(0xFF7986CB)
+val CalendarGraphite = Color(0xFF616161)
 // val CalendarGrape = Color(0xFF8E24AA)
 // val CalendarFlamingo = Color(0xFFE67C73)
 // val CalendarBanana = Color(0xFFF6BF26)
@@ -18,10 +21,3 @@ val Pink40 = Color(0xFF7D5260)
 // val CalendarBlueberry = Color(0xFF3F51B5)
 // val CalendarBasil = Color(0xFF0B8043)
 // val CalendarTomato = Color(0xFFD50000)
-
-val MainColor = Color(0xFF3B71CA)
-val DarkMainColor = Color(0xFF3B71CA)
-
-val CalendarSage = Color(0xFF33B679)
-val CalendarLavender = Color(0xFF7986CB)
-val CalendarGraphite = Color(0xFF616161)
--- app/src/main/java/pnu/plato/calendar/presentation/common/theme/Theme.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/common/theme/Theme.kt
@@ -11,25 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = DarkMainColor,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimaryColor,
+    onPrimary = DarkOnPrimaryColor,
+    background = DarkBackgroundColor,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = MainColor,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor,
+    onPrimary = OnPrimaryColor,
+    background = BackgroundColor,
 )
 
 @Composable
