91f3d90324f56feed48163c9339ea238d7bfe19a
dpcks0509 <dpcks0509@gmail.com>
2025-09-17 13:18:56 +0900

    refactor: CalendarScreen import

--- app/src/main/java/pnu/plato/calendar/presentation/common/navigation/BottomBarItem.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/common/navigation/BottomBarItem.kt
@@ -7,6 +7,9 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import pnu.plato.calendar.R
+import pnu.plato.calendar.presentation.common.navigation.PlatoCalendarScreen.CalendarScreen
+import pnu.plato.calendar.presentation.common.navigation.PlatoCalendarScreen.SettingScreen
+import pnu.plato.calendar.presentation.common.navigation.PlatoCalendarScreen.ToDoScreen
 
 enum class BottomBarItem(
     val route: PlatoCalendarScreen,
@@ -14,17 +17,17 @@ enum class BottomBarItem(
     @StringRes val titleRes: Int,
 ) {
     CALENDAR(
-        route = PlatoCalendarScreen.CalendarScreen,
+        route = CalendarScreen,
         icon = Icons.Default.DateRange,
         titleRes = R.string.calendar,
     ),
     TODO(
-        route = PlatoCalendarScreen.ToDoScreen,
+        route = ToDoScreen,
         icon = Icons.Default.List,
         titleRes = R.string.to_do,
     ),
     SETTING(
-        route = PlatoCalendarScreen.SettingScreen,
+        route = SettingScreen,
         icon = Icons.Default.Settings,
         titleRes = R.string.setting,
     ),
