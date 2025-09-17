2996d502a5efc6fcce41241e039221c1c89238d0
dpcks0509 <dpcks0509@gmail.com>
2025-09-17 10:53:22 +0900

    refactor: preview modifier 수정

--- app/src/main/java/pnu/plato/calendar/presentation/calendar/component/DayOfWeekHeader.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/calendar/component/DayOfWeekHeader.kt
@@ -3,10 +3,12 @@ package pnu.plato.calendar.presentation.calendar.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pnu.plato.calendar.presentation.calendar.model.DayOfWeekUiModel
 import pnu.plato.calendar.presentation.calendar.model.DayOfWeekUiModel.Companion.dayOfWeeksEntries
 import pnu.plato.calendar.presentation.common.theme.PlatoCalendarTheme
@@ -32,6 +34,11 @@ fun DayOfWeekHeader(modifier: Modifier = Modifier) {
 @Composable
 fun DayOfWeekHeaderPreview() {
     PlatoCalendarTheme {
-        DayOfWeekHeader(modifier = Modifier.fillMaxWidth())
+        DayOfWeekHeader(
+            modifier =
+                Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+        )
     }
 }
