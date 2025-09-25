db4afcd0b096c02b9e545da4879772d8c2010cbf
dpcks0509 <dpcks0509@gmail.com>
2025-09-25 23:34:55 +0900

    feat: CourseScheduleContent 구현

--- app/src/main/java/pnu/plato/calendar/presentation/calendar/component/bottomsheet/CourseScheduleContent.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/calendar/component/bottomsheet/CourseScheduleContent.kt
@@ -4,36 +4,49 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import pnu.plato.calendar.domain.entity.Schedule.PersonalSchedule.CustomSchedule
-import pnu.plato.calendar.presentation.calendar.model.ScheduleUiModel
 import pnu.plato.calendar.presentation.calendar.model.ScheduleUiModel.PersonalScheduleUiModel.CourseScheduleUiModel
 import pnu.plato.calendar.presentation.common.extension.noRippleClickable
 import pnu.plato.calendar.presentation.common.theme.Black
 import pnu.plato.calendar.presentation.common.theme.Gray
+import pnu.plato.calendar.presentation.common.theme.LightGray
 import pnu.plato.calendar.presentation.common.theme.PrimaryColor
 import pnu.plato.calendar.presentation.common.theme.White
-import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+private const val HAS_NO_DESCRIPTION = "설명 없음"
 
 @Composable
 fun CourseScheduleContent(
@@ -41,11 +54,14 @@ fun CourseScheduleContent(
     editSchedule: (CustomSchedule) -> Unit,
     onDismissRequest: () -> Unit,
 ) {
-    var title: String by remember { mutableStateOf(schedule.title) }
-    var description: String by remember { mutableStateOf(schedule.description.orEmpty()) }
-    var startAt: LocalDateTime by remember { mutableStateOf(schedule.startAt) }
-    var endAt: LocalDateTime by remember { mutableStateOf(schedule.endAt) }
-    var isCompleted: Boolean by remember { mutableStateOf(schedule.isCompleted) }
+    val dateFormatter = DateTimeFormatter.ofPattern("M월 d일 (E)", Locale.KOREAN)
+    val timeFormatter = DateTimeFormatter.ofPattern("a h:mm", Locale.KOREAN)
+    val formattedStartDate = schedule.startAt.format(dateFormatter)
+    val formattedStartTime = schedule.startAt.format(timeFormatter)
+    val formattedEndDate = schedule.endAt.format(dateFormatter)
+    val formattedEndTime = schedule.endAt.format(timeFormatter)
+    val formattedStartYear = "${schedule.startAt.year}년"
+    val formattedEndYear = "${schedule.endAt.year}년"
 
     Row(
         modifier =
@@ -66,24 +82,208 @@ fun CourseScheduleContent(
                     .size(32.dp)
                     .noRippleClickable(onDismissRequest),
         )
+    }
 
-        ActionButton(
-            text = "수정",
-            onClick = {
-                if (title.isNotEmpty()) {
-                    editSchedule(
-                        CustomSchedule(
-                            id = schedule.id,
-                            title = title,
-                            description = description,
-                            startAt = startAt,
-                            endAt = endAt,
-                            isCompleted = isCompleted,
-                        ),
-                    )
-                }
-            },
+    Spacer(modifier = Modifier.height(12.dp))
+
+    Row(
+        modifier =
+            Modifier
+                .padding(horizontal = 12.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .shadow(
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    clip = true,
+                    ambientColor = Black,
+                    spotColor = Black
+                )
+                .background(White),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Box(
+            modifier =
+                Modifier
+                    .width(4.dp)
+                    .fillMaxHeight()
+                    .padding(vertical = 12.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(schedule.color),
         )
+
+        TextField(
+            value = schedule.title,
+            readOnly = true,
+            onValueChange = {},
+            textStyle =
+                TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Black,
+                ),
+            colors =
+                TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    cursorColor = schedule.color,
+                ),
+            maxLines = 3,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+
+    Spacer(modifier = Modifier.height(12.dp))
+
+    Column(
+        modifier =
+            Modifier
+                .padding(horizontal = 12.dp)
+                .fillMaxWidth()
+                .shadow(
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    clip = true,
+                    ambientColor = Black,
+                    spotColor = Black
+                )
+                .background(White)
+                .padding(vertical = 18.dp, horizontal = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Description",
+                tint = Black,
+                modifier = Modifier.size(24.dp),
+            )
+
+            TextField(
+                value = schedule.description.orEmpty(),
+                readOnly = true,
+                onValueChange = { },
+                placeholder = {
+                    Text(
+                        text = HAS_NO_DESCRIPTION,
+                        fontSize = 16.sp,
+                        color = Gray,
+                    )
+                },
+                textStyle =
+                    TextStyle(
+                        fontSize = 16.sp,
+                        color = Black,
+                    ),
+                colors =
+                    TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        cursorColor = schedule.color,
+                    ),
+                maxLines = 5,
+                modifier = Modifier.weight(1f),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = "Date",
+                tint = Black,
+                modifier = Modifier.size(24.dp),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier =
+                    Modifier
+                        .weight(1f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(LightGray)
+                        .padding(vertical = 8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    text = formattedStartYear,
+                    fontSize = 14.sp,
+                    color = Gray,
+                )
+                Text(
+                    text = formattedStartDate,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Black,
+                )
+                Text(
+                    text = formattedStartTime,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Black,
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = "Arrow",
+                tint = Black,
+                modifier = Modifier.size(24.dp),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier =
+                    Modifier
+                        .weight(1f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(LightGray)
+                        .padding(vertical = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    text = formattedEndYear,
+                    fontSize = 14.sp,
+                    color = Gray,
+                )
+                Text(
+                    text = formattedEndDate,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Black,
+                )
+                Text(
+                    text = formattedEndTime,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Black,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
     }
 
     Spacer(modifier = Modifier.height(24.dp))
@@ -133,5 +333,5 @@ fun CourseScheduleContent(
         )
     }
 
-    Spacer(modifier = Modifier.height(18.dp))
+    Spacer(modifier = Modifier.height(24.dp))
 }
