92eeb2803c5e8733edd32be732c9a298797f0b59
dpcks0509 <dpcks0509@gmail.com>
2025-10-02 10:05:33 +0900

    feat: 알림 시간 6시간, 12시간 추가

--- app/src/main/java/pnu/plato/calendar/presentation/setting/model/NotificationTime.kt
+++ app/src/main/java/pnu/plato/calendar/presentation/setting/model/NotificationTime.kt
@@ -6,6 +6,8 @@ enum class NotificationTime(
     NONE("없음"),
     ONE_HOUR("1시간 전"),
     TWO_HOURS("2시간 전"),
+    SIX_HOURS("6시간 전"),
+    TWELVE_HOURS("12시간 전"),
     ONE_DAY("1일 전"),
     TWO_DAYS("2일 전"),
     ONE_WEEK("1주 전"),
