6bb9c5aa36a2021861dba83a83c3b3da80820e92
dpcks0509 <dpcks0509@gmail.com>
2025-11-13 17:56:33 +0900

    feat: 불필요한 주석 제거

--- app/src/main/java/pusan/university/plato_calendar/data/local/repository/LocalCourseRepository.kt
+++ app/src/main/java/pusan/university/plato_calendar/data/local/repository/LocalCourseRepository.kt
@@ -11,31 +11,32 @@ import javax.inject.Inject
 private const val UNKNOWN_COURSE_NAME = "알 수 없는 교과목"
 
 class LocalCourseRepository
-@Inject
-constructor(
-    @ApplicationContext context: Context,
-) : CourseRepository {
-    private val courses: Map<String, String> by lazy {
-        val jsonString =
-            context.assets
-                .open("courses.json")
-                .bufferedReader()
-                .use { it.readText() }
+    @Inject
+    constructor(
+        @ApplicationContext context: Context,
+    ) : CourseRepository {
+        private val courses: Map<String, String> by lazy {
+            val jsonString =
+                context.assets
+                    .open("courses.json")
+                    .bufferedReader()
+                    .use { it.readText() }
 
-        // Create a concrete type to avoid ProGuard issues
-        val type = object : TypeToken<Map<String, String>>() {}.type
-        Gson().fromJson(jsonString, type)
+            val type = object : TypeToken<Map<String, String>>() {}.type
+            Gson().fromJson(jsonString, type)
+        }
+
+        override fun getCourseName(courseCode: String): String =
+            courses.entries
+                .find { course ->
+                    course.key.formatCourseCode() == courseCode
+                }?.value ?: UNKNOWN_COURSE_NAME
+
+        override fun getCourseCode(courseName: String): String =
+            courses.entries
+                .find { course ->
+                    course.value == courseName
+                }?.key
+                ?.formatCourseCode()
+                .orEmpty()
     }
-
-    override fun getCourseName(courseCode: String): String =
-        courses.entries
-            .find { course ->
-                course.key.formatCourseCode() == courseCode
-            }?.value ?: UNKNOWN_COURSE_NAME
-
-    override fun getCourseCode(courseName: String): String =
-        courses.entries
-            .find { course ->
-                course.value == courseName
-            }?.key?.formatCourseCode().orEmpty()
-}
